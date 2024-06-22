6b58febe202b9201fefc73e69cd5e08b9f0df050
John <jng9230@gmail.com>
2024-06-22 19:00:44 -0400

    order cont., move menu data into utils

--- frontend/src/routes/Order.tsx
+++ frontend/src/routes/Order.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
 import Header from '../components/Header'
+import { itemToLinkDict, menuItems, MenuItem } from "../utils/menu-data"
+import tempTea from "../imgs/temp-tea.png"
+
 function Order() {
+    const handleAddCart = (item: MenuItem) => {
+        console.log(item)
+    }
     return (
         <>
             <Header />
-            <div>Order sth idk</div>
+            {
+                Object.values(itemToLinkDict).map(d => {
+                    const itemsInGroup = menuItems[d];
+                    //TODO: captalize the category name correctly
+                    const itemElements = itemsInGroup.map(d1 => {
+                        return <>
+                            <div>
+                                <img className="" src={d1.url || tempTea} alt={d1.description || d1.item} />
+                                <p className="font-bold"> {d1.item}</p>
+                                <p className=""> {d1.costInPeso} </p>
+                                {/* <button className="btn-std" onClick={() => handleAddCart(d1)}> Add to cart </button> */}
+                            </div>
+                        </>
+                    })
+                    return <>
+                        <div className="px-9 my-9">
+                            <h3 className="text-3xl my-8"> {d} </h3>
+                            <div className="grid grid-cols-2 md:grid-cols-4 grid-flow-dense gap-4">
+                                {itemElements}
+                            </div>
+                        </div>
+                    </>
+                })
+            }
         </>
     )
 }
---
+++ frontend/src/utils/menu-data.ts
@@ -0,0 +1,147 @@
+//TODO: enforce types on menu items???
+// BUT if we just keep everything tidy and in this file it's ok?
+
+// enum MenuGroupDR {
+//     BebidasRegular,
+//     Especialidades,
+//     BoloBao,
+//     PuffPuff,
+//     FusionOrientalSandwich
+// }
+
+// type MenuGroupDRStrings = keyof typeof MenuGroupDR
+
+// export const itemToLinkDict1: { [key in MenuGroupDRStrings]: string } = {
+//     "BebidasRegular": "beverages",
+//     "Especialidades": "specials",
+//     "BoloBao": "bolobao",
+//     "PuffPuff": "puffpuff",
+//     "FusionOrientalSandwich": "sandwich"
+// }
+
+
+export const itemToLinkDict = {
+    "Bebidas Regular": "beverages",
+    "Especialidades": "specials",
+    "Bolo Bao": "bolobao",
+    "Puff Puff": "puffpuff",
+    "Fusion Oriental Sandwich": "sandwich"
+}
+
+export type MenuItem = {
+    item: string,
+    costInPeso: number,
+    url?: string,
+    description?: string
+}
+
+export type MenuGroup = { [groupName: string]: MenuItem[] }
+
+export const menuItems: MenuGroup = {
+    "beverages": [
+        {
+            item: "Te Negro Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Te Verde Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Oolong Te Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Lychee Te Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Mango Te Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Chinola Te Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Durazno Te Latte",
+            costInPeso: 200,
+        },
+        {
+            item: "Manzana Te Latte",
+            costInPeso: 200,
+        }
+    ],
+    "specials": [
+        {
+            item: "Sol Citrico Te Verde",
+            costInPeso: 280,
+        },
+        {
+            item: "Fresco Limonada Te Verde",
+            costInPeso: 250,
+        },
+        {
+            item: "Fresca Fresa Te Verde",
+            costInPeso: 250,
+        },
+        {
+            item: "Matcha-Fresa Latte",
+            costInPeso: 250,
+        },
+        {
+            item: "Tiger Sugar Latte",
+            costInPeso: 280,
+        },
+        {
+            item: "Taro Queso Espuma Latte",
+            costInPeso: 250,
+        },
+    ],
+    "sandwich": [
+        {
+            item: "Pollo",
+            costInPeso: 350
+        },
+        {
+            item: "Cerdo",
+            costInPeso: 400
+        }
+    ],
+    "bolobao": [
+        {
+            item: "Queso crema",
+            costInPeso: 80
+        },
+        {
+            item: "Crema du Huevo",
+            costInPeso: 80
+        },
+        {
+            item: "Pina",
+            costInPeso: 80
+        },
+        {
+            item: "Coco Rallado",
+            costInPeso: 80
+        }
+    ],
+    "puffpuff": [
+        {
+            item: "Vanilla",
+            costInPeso: 100
+        },
+        {
+            item: "Chocolate",
+            costInPeso: 120
+        },
+        {
+            item: "Tiger Sugar",
+            costInPeso: 130
+        },
+        {
+            item: "Matcha",
+            costInPeso: 130
+        }
+    ]
+}
