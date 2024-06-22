d4713c512bf1bccfbc520d3672190fe4ad702158
John <jng9230@gmail.com>
2024-06-22 18:06:35 -0400

    burger to leftside lol

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import logo from "../imgs/logo-small.png"
-import { slide as Menu } from 'react-burger-menu'
+import { slide as BurgerMenu } from 'react-burger-menu'
 import "./burger-styles.css"
 
 function Header() {
     return (
         <header className="m-0 grid">
-            <Menu right>
+            <BurgerMenu>
                 <a id="home" className="menu-item" href="/">Home</a>
                 <a id="menu" className="menu-item" href="/menu">Menu</a>
                 <a id="order" className="menu-item" href="/order">Order</a>
-            </Menu>
+            </BurgerMenu>
             <a href="/" className="inline-block justify-self-center m-2">
                 <img src={logo} />
             </a>
